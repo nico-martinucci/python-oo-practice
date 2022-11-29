688bd67fd47f3511dde53e94252337404721d464
Nico Martinucci <domenico.martinucci@gmail.com>
2022-11-29 15:54:55 -0800

    add SPECIAL class

--- wordfinder.py
+++ wordfinder.py
@@ -4,13 +4,36 @@ class WordFinder:
     """Word Finder: finds random words from a dictionary."""
 
     def __init__(self, path):
-        # learn how to get file path
+        """Constructor for new word finder based on file at path"""
         file = open(path)
         self.words = file.read().splitlines()
+        print(f"{len(self.words)} words read")
+        # call self.method that prints word length
 
-        print(self.words)
+    def __repr__(self):
+        return f"<>"
 
     def random(self):
-        #from self.words, invoke randInt
+        """returns random word from instance's words property"""
         randomNum = randint(0, len(self.words)-1)
         return self.words[randomNum]
+    # method that prints word length -- "A"
+    def report_list_length(self):
+        
+
+class SpecialWordFinder(WordFinder):
+    """SPECIAL Word Finder!"""
+
+    def __init__(self, path):
+        """Constructor for new SPECIAL word finder, based on file at path"""
+        super().__init__(path)
+        self.scrub_blanks_and_hashes()
+    
+    def scrub_blanks_and_hashes(self):
+        """Scrubs out all empty lines and lines starting with '#'"""
+        self.words = [
+            word for word in self.words 
+            if word and (not word[0] == "#")
+        ]
+
+    # redefine method "A" here
